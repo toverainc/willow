3be84f8d9540d7f204dfd3037cda576b76f2eb24
Kristian Kielhofner <kris@tovera.com>
2023-03-31 14:27:43 -0500

    Update path stuff

--- utils.sh
+++ utils.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-ADF_PATH=/Users/kris/projects/willow/esp/esp-adf
-PORT="/dev/cu.usbserial-31320"
+export ADF_PATH=/Users/kris/projects/willow/esp/esp-adf
+export PORT="/dev/cu.usbserial-31320"
 
 case $1 in
 
