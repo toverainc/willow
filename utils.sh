cf4d29d0dcbefce131128770e9cc9b310ca5903d
Kristian Kielhofner <kris@tovera.com>
2023-03-31 20:26:28 -0500

    Add setup stuff and config updates

--- utils.sh
+++ utils.sh
@@ -1,7 +1,23 @@
 #!/bin/bash
+set -e # bail on error
 
-export ADF_PATH=/Users/kris/projects/willow/esp/esp-adf
-export PORT="/dev/cu.usbserial-31320"
+ADF_VER="v2.4.1"
+PLATFORM="all" # Why not?
+
+export ADF_PATH="$PWD/deps/esp-adf"
+export IDF_PATH="$ADF_PATH/esp-idf"
+
+# TODO: Figure out how to do this cross-platform or read a config or something
+export PORT="/dev/ttyUSB0"
+
+# Pull in environment
+if [ -r $IDF_PATH/export.sh ]; then
+    . $IDF_PATH/export.sh > /dev/null
+fi
+
+echo "
+If you are running a command that has a serial monitor you can exit with CTRL + ]
+"
 
 case $1 in
 
@@ -23,6 +39,45 @@ flash)
 
 monitor)
     idf.py -p "$PORT" monitor
+;;
+
+destroy)
+    echo "YOU ARE ABOUT TO REMOVE THIS ENTIRE ENVIRONMENT AND RESET THE REPO. HIT ENTER TO CONFIRM"
+    read -n
+    git reset --hard
+    git clean -fdx
+    rm -rf ~/.espressif deps
+    echo "Not a trace left. You will have to run setup again"
+;;
+
+setup)
+    mkdir -p deps
+    cd deps
+    # Setup ADF
+    git clone --recursive -b "$ADF_VER" https://github.com/espressif/esp-adf.git
+    cd esp-adf/esp-idf
+    ./install.sh "$PLATFORM"
+
+    # This is ridiculous
+    cd $IDF_PATH
+    git apply $ADF_PATH/idf_patches/idf_v4.4_freertos.patch
+
+    # Configure WiFi
+
+    echo "Please enter the SSID you would like to connect to"
+    read SSID
+
+    echo "Please enter the passphrase for SSID $SSID"
+    read KEY
+
+    #sed -i 's/old-text/new-text/g' input.txt
+    #sed -i 's/old-text/new-text/g' input.txt
+;;
+
+*)
+    echo "Passing args to idf.py as-is"
+    shift
+    idf.py "$@"
 ;;
 
 esac
