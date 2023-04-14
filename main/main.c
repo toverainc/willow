91ebbc9713746bd857c3809381e4bc7853a9346d
Stijn Tintel <stijn@linux-ipv6.be>
2023-04-14 22:14:35 +0200

    main: init I2S

--- main/main.c
+++ main/main.c
@@ -44,6 +44,7 @@ static const char *TAG = "SALLOW";
 #define AUDIO_BITS         (16)
 #define AUDIO_CHANNELS     (1)
 
+#define I2S_PORT I2S_NUM_0
 #define WAKENET_NAME "wn9_hiesp"
 
 #define SALLOW_EXIT_BIT (BIT0)
@@ -210,6 +211,42 @@ static void init_afe_data(void)
     ESP_LOGI(TAG, "if_afe: '%p'", if_afe);
 }
 
+static esp_err_t init_i2s(void)
+{
+    esp_err_t ret = ESP_OK;
+    i2s_config_t cfg_i2s = {
+        .bits_per_chan          = I2S_BITS_PER_CHAN_32BIT,
+        .bits_per_sample        = I2S_BITS_PER_SAMPLE_32BIT,
+        .channel_format         = I2S_CHANNEL_FMT_RIGHT_LEFT,
+        .communication_format   = I2S_COMM_FORMAT_STAND_I2S,
+        .dma_buf_count          = 6,
+        .dma_buf_len            = 160,
+        .fixed_mclk             = 0,
+        .intr_alloc_flags       = ESP_INTR_FLAG_LEVEL1,
+        .mclk_multiple          = I2S_MCLK_MULTIPLE_DEFAULT,
+        .mode                   = I2S_MODE_MASTER | I2S_MODE_RX | I2S_MODE_TX,
+        .sample_rate            = 16000,
+        .tx_desc_auto_clear     = true,
+        .use_apll               = false,
+    };
+
+    i2s_pin_config_t pcfg_i2s = {
+        .bck_io_num = GPIO_NUM_17,
+        .ws_io_num = GPIO_NUM_47,
+        .data_out_num = GPIO_NUM_15,
+        .data_in_num = GPIO_NUM_16,
+        .mck_io_num = GPIO_NUM_2,
+    };
+
+    ret = i2s_driver_install(I2S_PORT, &cfg_i2s, 0, NULL);
+    ESP_LOGI(TAG, "i2s_driver_install: %s", esp_err_to_name(ret));
+
+    ret = i2s_set_pin(I2S_PORT, &pcfg_i2s);
+    ESP_LOGI(TAG, "i2s_set_pin: %s", esp_err_to_name(ret));
+
+    return ret;
+}
+
 static esp_err_t init_sr_model()
 {
     char *wakenet_name = WAKENET_NAME;
@@ -359,6 +396,7 @@ void app_main(void)
     i2s_stream_set_clk(i2s_stream_writer, AUDIO_SAMPLE_RATE, AUDIO_BITS, AUDIO_CHANNELS);
 
     init_afe_data();
+    init_i2s();
 
     // Init LED last so user knows we are ready
     ESP_LOGI(TAG, "[ 3.5 ] Init LED so user knows we are ready");
