67197058617e267d93c74d7074a6b33a069aadbb
Kristian Kielhofner <kris@tovera.com>
2023-05-07 14:46:44 -0500

    Remove LVGL handling from tasks
    Standardize on clearing display on wake word

--- main/tasks.c
+++ main/tasks.c
@@ -18,10 +18,6 @@ void task_timer(void *data)
         vTaskDelay(value * 1000 / portTICK_PERIOD_MS);
         ESP_LOGI(TAG, "Wake LCD timeout, turning off LCD");
         ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 0);
-        lv_obj_add_flag(lbl_ln1, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(lbl_ln2, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(lbl_ln3, LV_OBJ_FLAG_HIDDEN);
-        lv_obj_add_flag(lbl_ln4, LV_OBJ_FLAG_HIDDEN);
         timer_pause(TIMER_GROUP_0, TIMER_0);
     }
 }
