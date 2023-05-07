df8de02f62f657fb8c9bc481a6c3a5a7ea3d4f15
Kristian Kielhofner <kris@tovera.com>
2023-05-07 13:40:33 -0500

    Add logging for turning off display, turn off display before LVGL clear

--- main/tasks.c
+++ main/tasks.c
@@ -16,11 +16,12 @@ void task_timer(void *data)
         ESP_LOGI(TAG, "received event in timer queue");
         // sleep value seconds
         vTaskDelay(value * 1000 / portTICK_PERIOD_MS);
+        ESP_LOGI(TAG, "Wake LCD timeout, turning off LCD");
+        ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 0);
         lv_obj_add_flag(lbl_ln1, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(lbl_ln2, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(lbl_ln3, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(lbl_ln4, LV_OBJ_FLAG_HIDDEN);
-        ledc_set_duty_and_update(LEDC_LOW_SPEED_MODE, LEDC_CHANNEL_1, 0, 0);
         timer_pause(TIMER_GROUP_0, TIMER_0);
     }
 }
