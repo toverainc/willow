03f81217d2cc8d2bb5c9dc7a57c536abdfe6cafd
Kristian Kielhofner <kris@tovera.com>
2023-05-14 10:21:31 -0500

    Wrap printing of multinet command size in ifdef for only if multinet enabled

--- main/main.c
+++ main/main.c
@@ -813,7 +813,9 @@ void app_main(void)
         }
     }
 
+#ifdef CONFIG_WILLOW_USE_MULTINET
     ESP_LOGI(TAG, "cmd_multinet[] size: %u bytes", get_cmd_multinet_size());
+#endif
     ESP_LOGI(TAG, "Startup complete. Waiting for wake word.");
 
     ESP_ERROR_CHECK_WITHOUT_ABORT(timer_start(TIMER_GROUP_0, TIMER_0));
