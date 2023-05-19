73256632f683650e0d4f9877269260e27c0fb059
Stijn Tintel <stijn@linux-ipv6.be>
2023-05-19 19:32:15 +0300

    http: abstract HTTP client code from hass.c
    This abstracts most HTTP client code from hass.c so we can reuse it in
    future command endpoint implementations.

---
+++ main/http.h
@@ -0,0 +1,3 @@
+esp_http_client_handle_t init_http_client(void);
+esp_err_t http_get(esp_http_client_handle_t hdl_hc, char *url, char **body, int *http_status);
+esp_err_t http_post(esp_http_client_handle_t hdl_hc, char *url, char *data, char **body, int *http_status);
