853d62e816c3faceaf2483813ee1a5fa47d3fb47
Stijn Tintel <stijn@linux-ipv6.be>
2023-05-19 16:03:05 +0300

    hass: check for speech in intent responses
    Check for speech intent responses, and print it on the display rather
    than "Success!" or "Error!".

--- main/main.c
+++ main/main.c
@@ -737,7 +737,9 @@ void app_main(void)
         lv_obj_align(lbl_ln3, LV_ALIGN_CENTER, 0, 0);
         lv_obj_align(lbl_ln4, LV_ALIGN_TOP_LEFT, 0, 150);
         lv_label_set_long_mode(lbl_ln2, LV_LABEL_LONG_SCROLL);
+        lv_label_set_long_mode(lbl_ln4, LV_LABEL_LONG_SCROLL);
         lv_obj_set_width(lbl_ln2, 320);
+        lv_obj_set_width(lbl_ln4, 320);
 #ifdef CONFIG_WILLOW_USE_MULTINET
         lv_label_set_text_static(lbl_ln3, "Starting up (local)...");
 #else
