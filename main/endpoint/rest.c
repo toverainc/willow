83ed4970e49334fa10cf07af352fca3985e52832
Stijn Tintel <stijn@linux-ipv6.be>
2023-05-20 17:39:12 +0300

    endpoint: add generic REST endpoint

---
+++ main/endpoint/rest.c
@@ -0,0 +1,52 @@
+#include "audio_hal.h"
+#include "audio_thread.h"
+#include "driver/timer.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "esp_lvgl_port.h"
+#include "lvgl.h"
+
+#include "http.h"
+#include "shared.h"
+#include "slvgl.h"
+
+void rest_send(char *data)
+{
+    bool ok = false;
+    char *body = NULL;
+    char *url = CONFIG_WILLOW_ENDPOINT_REST_URL;
+    esp_err_t ret;
+    int http_status;
+
+    esp_http_client_handle_t hdl_hc = init_http_client();
+
+    ESP_LOGI(TAG, "sending '%s' to REST API on '%s'", data, url);
+
+    ret = http_post(hdl_hc, url, data, &body, &http_status);
+    if (ret == ESP_OK) {
+        if (http_status == 200) {
+            ok = true;
+        }
+    } else {
+        ESP_LOGE(TAG, "failed to read HTTP POST response");
+    }
+
+    if (ok) {
+        audio_thread_create(NULL, "play_tone_ok", play_tone_ok, NULL, 4 * 1024, 10, true, 1);
+    } else {
+        audio_thread_create(NULL, "play_tone_err", play_tone_err, NULL, 4 * 1024, 10, true, 1);
+    }
+
+    lvgl_port_lock(0);
+    lv_obj_clear_flag(lbl_ln3, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_clear_flag(lbl_ln4, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_align(lbl_ln3, LV_ALIGN_TOP_LEFT, 0, 120);
+    lv_label_set_text_static(lbl_ln3, "Command status:");
+    lv_obj_remove_event_cb(lbl_ln3, cb_btn_cancel);
+    lv_label_set_text(lbl_ln4, ok ? "#008000 Success!" : "#ff0000 Error");
+    lvgl_port_unlock();
+
+    timer_start(TIMER_GROUP_0, TIMER_0);
+
+    free(body);
+}
