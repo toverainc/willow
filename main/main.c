toverainc/willow

commit 83ed4970e49334fa10cf07af352fca3985e52832

endpoint: add generic REST endpoint

main/main.c
@@ -51,6 +51,10 @@
 #include "endpoint/hass.h"
 #endif
 
+#ifdef CONFIG_WILLOW_USE_ENDPOINT_REST
+#include "endpoint/rest.h"
+#endif
+
 #define I2S_PORT I2S_NUM_0
 
 static bool recording = false;
@@ -173,8 +177,10 @@ static esp_err_t cb_ar_event(audio_rec_evt_t are, void *data)
             json = malloc(29 + strlen(lookup_cmd_multinet(command_id)));
             snprintf(json, 29 + strlen(lookup_cmd_multinet(command_id)), "{\"text\":\"%s\",\"language\":\"en\"}",
                      lookup_cmd_multinet(command_id));
-#ifdef CONFIG_WILLOW_USE_ENDPOINT_HOMEASSISTANT
+#if defined(CONFIG_WILLOW_USE_ENDPOINT_HOMEASSISTANT)
             hass_send(json);
+#elif defined(CONFIG_WILLOW_USE_ENDPOINT_REST)
+            rest_send(buf);
 #endif
             free(json);
 
@@ -309,8 +315,10 @@ esp_err_t hdl_ev_hs(http_stream_event_msg_t *msg)
             }
             buf[read_len] = 0;
             ESP_LOGI(TAG, "Got HTTP Response = %s", (char *)buf);
-#ifdef CONFIG_WILLOW_USE_ENDPOINT_HOMEASSISTANT
+#if defined(CONFIG_WILLOW_USE_ENDPOINT_HOMEASSISTANT)
             hass_send(buf);
+#elif defined(CONFIG_WILLOW_USE_ENDPOINT_REST)
+            rest_send(buf);
 #endif
 
             cJSON *cjson = cJSON_Parse(buf);

main/CMakeLists.txt
@@ -23,6 +23,8 @@ set(COMPONENT_PRIV_REQUIRES
 
 if(CONFIG_WILLOW_USE_ENDPOINT_HOMEASSISTANT)
 	set(COMPONENT_SRCS ${COMPONENT_SRCS} "endpoint/hass.c")
+elseif(CONFIG_WILLOW_USE_ENDPOINT_REST)
+	set(COMPONENT_SRCS "${COMPONENT_SRCS}" "endpoint/rest.c")
 endif()
 
 register_component()

main/Kconfig.projbuild
@@ -46,6 +46,11 @@ choice WILLOW_USE_ENDPOINT
             bool "Home Assistant"
             help
                 Send command inferred from speech to Home Assistant
+
+        config WILLOW_USE_ENDPOINT_REST
+            bool "Generic REST endpoint"
+            help
+                Send command inferred from speech to generic REST endpoint
 endchoice
 
 config WILLOW_USE_MULTINET
@@ -80,6 +85,13 @@ config HOMEASSISTANT_TOKEN
     string "HomeAssistant Auth Token"
     help
         Long Lived Auth Token for Home Assistant.
+
+config WILLOW_ENDPOINT_REST_URL
+    depends on WILLOW_USE_ENDPOINT_REST
+    string "Generic REST endpoint URL"
+    help
+        URL for the generic REST endpoint. Use the complete URL!
+        E.g.: https://generic.tovera.io/api/rest
 
 config WILLOW_TIMEZONE
     string "Timezone"

main/endpoint/rest.c
@@ -1,0 +1,52 @@
+#include "audio_hal.h"
+#include "audio_thread.h"
+#include "driver/timer.h"
+#include "esp_http_client.h"
+#include "esp_log.h"
+#include "esp_lvgl_port.h"
+#include "lvgl.h"
+
+#include "http.h"
+#include "shared.h"
+#include "slvgl.h"
+
+void rest_send(char *data)
+{
+    bool ok = false;
+    char *body = NULL;
+    char *url = CONFIG_WILLOW_ENDPOINT_REST_URL;
+    esp_err_t ret;
+    int http_status;
+
+    esp_http_client_handle_t hdl_hc = init_http_client();
+
+    ESP_LOGI(TAG, "sending '%s' to REST API on '%s'", data, url);
+
+    ret = http_post(hdl_hc, url, data, &body, &http_status);
+    if (ret == ESP_OK) {
+        if (http_status == 200) {
+            ok = true;
+        }
+    } else {
+        ESP_LOGE(TAG, "failed to read HTTP POST response");
+    }
+
+    if (ok) {
+        audio_thread_create(NULL, "play_tone_ok", play_tone_ok, NULL, 4 * 1024, 10, true, 1);
+    } else {
+        audio_thread_create(NULL, "play_tone_err", play_tone_err, NULL, 4 * 1024, 10, true, 1);
+    }
+
+    lvgl_port_lock(0);
+    lv_obj_clear_flag(lbl_ln3, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_clear_flag(lbl_ln4, LV_OBJ_FLAG_HIDDEN);
+    lv_obj_align(lbl_ln3, LV_ALIGN_TOP_LEFT, 0, 120);
+    lv_label_set_text_static(lbl_ln3, "Command status:");
+    lv_obj_remove_event_cb(lbl_ln3, cb_btn_cancel);
+    lv_label_set_text(lbl_ln4, ok ? "#008000 Success!" : "#ff0000 Error");
+    lvgl_port_unlock();
+
+    timer_start(TIMER_GROUP_0, TIMER_0);
+
+    free(body);
+}

main/endpoint/rest.h
@@ -1,0 +1,1 @@
+void rest_send(char *data);

commit 3eaab7e1f5a47d929614aee37b733693ca5419b2

main: fix REST endpoint with multinet
There is no buf variable in the cb_ar_event function.

Fixes: 83ed4970e493 ("endpoint: add generic REST endpoint")

main/main.c
@@ -180,7 +180,7 @@ static esp_err_t cb_ar_event(audio_rec_evt_t are, void *data)
 #if defined(CONFIG_WILLOW_USE_ENDPOINT_HOMEASSISTANT)
             hass_send(json);
 #elif defined(CONFIG_WILLOW_USE_ENDPOINT_REST)
-            rest_send(buf);
+            rest_send(json);
 #endif
             free(json);
 