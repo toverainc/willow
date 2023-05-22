7d306c6c90547e295255b653083542782e6de5c9
Stijn Tintel <stijn@linux-ipv6.be>
2023-05-23 00:44:21 +0300

    timer: add timer to force end stream
    Add a timer that sends MSG_STOP N seconds after starting, where N is the
    value of CONFIG_WILLOW_STREAM_TIMEOUT.
    
    We don't need this for the following case, audio_rec_cfg_t.wakeup_time
    already takes care of it:
    
    I (00:16:15.053) WILLOW: AUDIO_REC_WAKEUP_START
    
    I (00:16:25.053) WILLOW: AUDIO_REC_WAKEUP_END
    
    Default to 5s which should be long enough for most commands. And max
    timeout 30s which matches Whisper's default max without chunking.
    
    Closes #18.

--- main/timer.c
+++ main/timer.c
@@ -1,14 +1,26 @@
 #include "driver/timer.h"
+#include "esp_log.h"
 #include "freertos/FreeRTOS.h"
 #include "freertos/queue.h"
 
+#include "shared.h"
 #include "tasks.h"
 #include "timer.h"
 
 #define TIMER_DIV   (16)
 #define TIMER_SCALE (TIMER_BASE_CLK / TIMER_DIV)
 
+esp_timer_handle_t hdl_sess_timer = NULL;
 xQueueHandle hdl_q_timer = NULL;
+
+static void cb_session_timer(void *data)
+{
+    if (recording) {
+        ESP_LOGI(TAG, "session timer expired - forcing end stream");
+        int msg = MSG_STOP;
+        xQueueSend(q_rec, &msg, 0);
+    }
+}
 
 static bool IRAM_ATTR cb_timer_isr(void *data)
 {
@@ -45,4 +57,14 @@ void reset_timer(bool pause)
         timer_pause(TIMER_GROUP_0, TIMER_0);
     }
     timer_set_counter_value(TIMER_GROUP_0, TIMER_0, 0);
+}
+
+esp_err_t init_session_timer(void)
+{
+    const esp_timer_create_args_t cfg_et = {
+        .callback = &cb_session_timer,
+        .name = "session_timer",
+    };
+
+    return esp_timer_create(&cfg_et, &hdl_sess_timer);
 }
