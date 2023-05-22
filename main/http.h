25b1cba0e8bdb17e3223d920686bcc18b67becc1
Stijn Tintel <stijn@linux-ipv6.be>
2023-05-22 23:19:28 +0300

    http: constify function arguments

--- main/http.h
+++ main/http.h
@@ -1,4 +1,5 @@
 esp_http_client_handle_t init_http_client(void);
-esp_err_t http_get(esp_http_client_handle_t hdl_hc, char *url, char **body, int *http_status);
-esp_err_t http_post(esp_http_client_handle_t hdl_hc, char *url, char *ctype, char *data, char **body, int *http_status);
-esp_err_t http_set_basic_auth(esp_http_client_handle_t hdl_hc, char *username, char *password);
+esp_err_t http_get(const esp_http_client_handle_t hdl_hc, const char *url, char **body, int *http_status);
+esp_err_t http_post(const esp_http_client_handle_t hdl_hc, const char *url, const char *ctype, const char *data,
+                    char **body, int *http_status);
+esp_err_t http_set_basic_auth(const esp_http_client_handle_t hdl_hc, const char *username, const char *password);
