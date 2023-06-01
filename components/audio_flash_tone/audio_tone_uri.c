c07881f9e8e7795b790dc0e8c161fe6c2f1e266b
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-01 18:12:35 +0200

    treewide: fix indentation

--- components/audio_flash_tone/audio_tone_uri.c
+++ components/audio_flash_tone/audio_tone_uri.c
@@ -1,8 +1,8 @@
 /*This is tone file*/
 
 const char* tone_uri[] = {
-   "flash://tone/0_dingdong.mp3",
-   "flash://tone/1_haode.mp3",
+    "flash://tone/0_dingdong.mp3",
+    "flash://tone/1_haode.mp3",
 };
 
 int get_tone_uri_num()
