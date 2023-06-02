1cdbcf3467c7be9c3b506f1f9afc8d611b9e0ddc
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-02 21:08:05 +0200

    display: move init_display to separate file

--- main/main.c
+++ main/main.c
@@ -42,6 +42,7 @@
 #include "generated_cmd_multinet.h"
 #endif
 
+#include "display.h"
 #include "network.h"
 #include "shared.h"
 #include "slvgl.h"
@@ -71,7 +72,6 @@ static audio_pipeline_handle_t hdl_ap_to_api;
 static audio_rec_handle_t hdl_ar = NULL;
 
 QueueHandle_t q_rec;
-esp_lcd_panel_handle_t hdl_lcd;
 
 #if defined(CONFIG_WILLOW_AUDIO_RESPONSE_FS)
 static void play_audio_err(void *data)
@@ -630,58 +630,6 @@ static void at_read(void *data)
 
     free(buf);
     vTaskDelete(NULL);
-}
-
-static esp_err_t init_display(void)
-{
-    ESP_LOGD(TAG, "initializing display");
-
-    const ledc_channel_config_t cfg_bl_channel = {
-        .channel = LEDC_CHANNEL_1,
-        .duty = CONFIG_WILLOW_LCD_BRIGHTNESS,
-        .gpio_num = GPIO_NUM_45,
-        .hpoint = 0,
-        .intr_type = LEDC_INTR_DISABLE,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .timer_sel = 1,
-    };
-
-    const ledc_timer_config_t cfg_bl_timer = {
-        .clk_cfg = LEDC_AUTO_CLK,
-        .duty_resolution = LEDC_TIMER_10_BIT,
-        .freq_hz = 5000,
-        .speed_mode = LEDC_LOW_SPEED_MODE,
-        .timer_num = 1,
-    };
-
-    int ret = ESP_OK;
-
-    hdl_lcd = audio_board_lcd_init(hdl_pset, NULL);
-    ret = esp_lcd_panel_disp_off(hdl_lcd, false);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed to turn of display: %s", esp_err_to_name(ret));
-        return ret;
-    }
-
-    ret = ledc_timer_config(&cfg_bl_timer);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed to config LEDC timer for display backlight: %s", esp_err_to_name(ret));
-        return ret;
-    }
-
-    ret = ledc_channel_config(&cfg_bl_channel);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed to config LEDC channel for display backlight: %s", esp_err_to_name(ret));
-        return ret;
-    }
-
-    ret = ledc_fade_func_install(0);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed to install LEDC fade function: %s", esp_err_to_name(ret));
-        return ret;
-    }
-
-    return ret;
 }
 
 static esp_err_t init_buttons(void)
