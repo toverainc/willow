bcd8e6ff1131175fd72490da1703a1841fd269a8
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-14 18:44:13 +0300

    config: add config_get functions for types we use
    For this we need to make the cJSON pointer a global variable.

--- main/config.h
+++ main/config.h
@@ -1,3 +1,9 @@
+#include "cJSON.h"
+
 bool config_valid;
+cJSON *wc;
+bool config_get_bool(char *key);
+char *config_get_char(char *key);
+int config_get_int(char *key);
 void config_parse(void);
 void config_write(const char *data);
