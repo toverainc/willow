eb0174264dfbb0ef28118675c2723610a72e5288
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-14 20:29:16 +0300

    rest: use config_get

--- main/endpoint/rest.c
+++ main/endpoint/rest.c
@@ -5,6 +5,7 @@
 #include "esp_lvgl_port.h"
 #include "lvgl.h"
 
+#include "config.h"
 #include "http.h"
 #include "shared.h"
 #include "slvgl.h"
@@ -14,26 +15,24 @@ void rest_send(const char *data)
 {
     bool ok = false;
     char *body = NULL;
-    char *url = CONFIG_WILLOW_ENDPOINT_REST_URL;
     esp_err_t ret;
     int http_status;
 
     esp_http_client_handle_t hdl_hc = init_http_client();
 
-#if defined(CONFIG_WILLOW_ENDPOINT_REST_AUTH_AUTH_HEADER)
-    ret = esp_http_client_set_header(hdl_hc, "Authorization", CONFIG_WILLOW_ENDPOINT_REST_AUTH_HEADER);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed to set authorization header: %s", esp_err_to_name(ret));
+    if (strcmp(config_get_char("rest_auth_type"), "Header") == 0) {
+        ret = esp_http_client_set_header(hdl_hc, "Authorization", config_get_char("rest_auth_header"));
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "failed to set authorization header: %s", esp_err_to_name(ret));
+        }
+    } else if (strcmp(config_get_char("rest_auth_type"), "Basic") == 0) {
+        ret = http_set_basic_auth(hdl_hc, config_get_char("rest_auth_user"), config_get_char("rest_auth_pass"));
+        if (ret != ESP_OK) {
+            ESP_LOGE(TAG, "failed to enable HTTP Basic Authentication: %s", esp_err_to_name(ret));
+        }
     }
-#elif defined(CONFIG_WILLOW_ENDPOINT_REST_AUTH_BASIC)
-    ret = http_set_basic_auth(hdl_hc, CONFIG_WILLOW_ENDPOINT_REST_AUTH_USERNAME,
-                              CONFIG_WILLOW_ENDPOINT_REST_AUTH_PASSWORD);
-    if (ret != ESP_OK) {
-        ESP_LOGE(TAG, "failed to enable HTTP Basic Authentication: %s", esp_err_to_name(ret));
-    }
-#endif
 
-    ret = http_post(hdl_hc, url, "application/json", data, &body, &http_status);
+    ret = http_post(hdl_hc, config_get_char("rest_url"), "application/json", data, &body, &http_status);
     if (ret == ESP_OK) {
         if (http_status >= 200 && http_status <= 299) {
             ok = true;
