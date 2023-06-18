14350c2bc85e1416925b92eeca71a1f823904061
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-18 20:37:03 +0300

    shared: move timer variables to timer.h
    In new source files we're defining a different TAG variable instead of
    using the one from shared.h. This causes redefinition errors if we
    include shared.h.
    
    Move timer variables to timer.h so we don't need to include shared.h to
    be able to control timers.

--- main/shared.h
+++ main/shared.h
@@ -3,14 +3,11 @@
 #include "esp_netif.h"
 #include "esp_peripherals.h"
 
-#define DISPLAY_TIMEOUT_US 10 * 1000 * 1000
-
 bool recording;
 
 esp_audio_handle_t hdl_ea;
 esp_lcd_panel_handle_t hdl_lcd;
 esp_periph_set_handle_t hdl_pset;
-esp_timer_handle_t hdl_display_timer, hdl_sess_timer;
 
 extern QueueHandle_t q_rec;
 
