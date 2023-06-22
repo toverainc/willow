9def8167e057229a78d2cde1a449f2fe37bb31c9
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-22 20:18:15 +0300

    treewide: use 5 lines instead of repositioning
    We're currently repositioning line 3 to print stuff in the middle of the
    screen. This requires us to keep track of when we have to reposition it
    again. To make our lives easier, just use 5 lines instead and use line 3
    for things that go in the middle.

--- main/slvgl.h
+++ main/slvgl.h
@@ -1,6 +1,6 @@
 extern esp_lcd_panel_handle_t hdl_lcd;
 extern lv_disp_t *ld;
-extern lv_obj_t *btn_cancel, *lbl_btn_cancel, *lbl_ln1, *lbl_ln2, *lbl_ln3, *lbl_ln4;
+extern lv_obj_t *btn_cancel, *lbl_btn_cancel, *lbl_ln1, *lbl_ln2, *lbl_ln3, *lbl_ln4, *lbl_ln5;
 
 void cb_btn_cancel(lv_event_t *);
 void cb_scr(lv_event_t *ev);
