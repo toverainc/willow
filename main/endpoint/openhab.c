84d6db435cbe6574529b5978446cadf02f1ffa03
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-22 20:18:15 +0300

    treewide: use 5 lines instead of repositioning
    We're currently repositioning line 3 to print stuff in the middle of the
    screen. This requires us to keep track of when we have to reposition it
    again. To make our lives easier, just use 5 lines instead and use line 3
    for things that go in the middle.

--- main/endpoint/openhab.c
+++ main/endpoint/openhab.c
@@ -65,16 +65,15 @@ end:
     }
 
     lvgl_port_lock(0);
-    lv_obj_clear_flag(lbl_ln3, LV_OBJ_FLAG_HIDDEN);
     lv_obj_clear_flag(lbl_ln4, LV_OBJ_FLAG_HIDDEN);
-    lv_obj_align(lbl_ln3, LV_ALIGN_TOP_LEFT, 0, 120);
-    lv_label_set_text_static(lbl_ln3, "Command status:");
-    lv_obj_remove_event_cb(lbl_ln3, cb_btn_cancel);
+    lv_obj_clear_flag(lbl_ln5, LV_OBJ_FLAG_HIDDEN);
+    lv_label_set_text_static(lbl_ln4, "Command status:");
+    lv_obj_remove_event_cb(lbl_ln4, cb_btn_cancel);
     if (strlen(body) > 1) {
         ESP_LOGI(TAG, "REST response: %s", body);
-        lv_label_set_text(lbl_ln4, body);
+        lv_label_set_text(lbl_ln5, body);
     } else {
-        lv_label_set_text(lbl_ln4, ok ? "#008000 Success!" : "#ff0000 Error");
+        lv_label_set_text(lbl_ln5, ok ? "#008000 Success!" : "#ff0000 Error");
     }
     lvgl_port_unlock();
 
