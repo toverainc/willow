479157359044aea217d2d5fe9f3fd2b62d9327db
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-23 18:44:33 +0300

    system: add hardware type and send in hello msg

--- main/system.h
+++ main/system.h
@@ -1,1 +1,12 @@
+enum willow_hw_t {
+    WILLOW_HW_UNSUPPORTED = 0,
+    WILLOW_HW_ESP32_S3_BOX,
+    WILLOW_HW_ESP32_S3_BOX_LITE,
+    WILLOW_HW_MAX, // keep this last
+};
+
+enum willow_hw_t hw_type;
+
+const char *str_hw_type(int id);
+void init_system(void);
 void restart_delayed(void);
