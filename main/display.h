a77655350c391da04db3113296d9a07d3a7fcee9
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-27 01:32:15 +0300

    shared: move hdl_lcd to display.h

--- main/display.h
+++ main/display.h
@@ -1,2 +1,6 @@
+#include "esp_lcd_types.h"
+
+esp_lcd_panel_handle_t hdl_lcd;
+
 esp_err_t init_display(void);
 void display_set_backlight(const bool on);
--- main/shared.h
+++ main/shared.h
@@ -1,6 +1,4 @@
-#include "esp_lvgl_port.h"
 #include "esp_netif.h"
 #include "esp_peripherals.h"
 
-esp_lcd_panel_handle_t hdl_lcd;
 esp_periph_set_handle_t hdl_pset;
