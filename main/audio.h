a8c36dc4d61a6acf3f954ffec2f0b17fd4602987
Stijn Tintel <stijn@linux-ipv6.be>
2023-06-27 01:25:16 +0300

    treewide: move audio related declarations from shared.h

--- main/audio.h
+++ main/audio.h
@@ -1,2 +1,20 @@
+#include "esp_audio.h"
+
+struct willow_audio_response {
+    void (*fn_err)(void *data);
+    void (*fn_ok)(void *data);
+};
+
+typedef enum {
+    MSG_STOP,
+    MSG_START,
+    MSG_START_LOCAL,
+} q_msg;
+
+bool recording;
+esp_audio_handle_t hdl_ea;
+extern QueueHandle_t q_rec;
+struct willow_audio_response war;
+
 void deinit_audio(void);
 void init_audio(void);
