12c15064be01e1d747e6cfff2dbf4f39167f5d9f
Stijn Tintel <stijn@linux-ipv6.be>
2023-09-05 17:02:58 +0300

    treewide: use commit hash for ESP-ADF
    We currently point to the willow branch of our ESP-ADF fork. As long as
    we don't update that branch, this is fine, but once this branch gets
    updated, rebuilding Willow would result in a different binary, even when
    building a tag. This makes bisecting near impossible.
    
    Use a commit hash instead of a branch name to avoid above problems.
    
    (cherry picked from commit 2d0b90a8d625c07ab24e147cf6711cdccdbfb700)

--- utils.sh
+++ utils.sh
@@ -172,8 +172,9 @@ install() {
     mkdir -p deps
     cd deps
     # Setup ADF
-    git clone -b "$ADF_VER" https://github.com/toverainc/esp-adf.git
+    git clone https://github.com/toverainc/esp-adf.git
     cd $ADF_PATH
+    git checkout "$ADF_VER"
     git submodule update --init components/esp-adf-libs
 
     # Setup esp-sr
