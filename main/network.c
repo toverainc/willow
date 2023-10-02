toverainc/willow

commit e9be16b291681f8beef717f2a59354fa14310e9d

network: use config_get for NTP

main/network.c
@@ -6,9 +6,13 @@
 #include "periph_wifi.h"
 #include "sdkconfig.h"
 
+#include "config.h"
 #include "network.h"
 #include "shared.h"
 #include "slvgl.h"
+
+#define DEFAULT_NTP_CONFIG "Host"
+#define DEFAULT_NTP_HOST   "pool.ntp.org"
 
 #define HOSTNAME_SIZE 20
 #define MAC_ADDR_SIZE 6
@@ -53,13 +57,18 @@ esp_err_t init_sntp(void)
     setenv("TZ", CONFIG_WILLOW_TIMEZONE, 1);
     tzset();
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
-#ifdef CONFIG_WILLOW_NTP_USE_DHCP
-    ESP_LOGI(TAG, "Using DHCP SNTP server");
-    sntp_servermode_dhcp(1);
-#else
-    ESP_LOGI(TAG, "Using configured SNTP server '%s'", CONFIG_WILLOW_NTP_HOST);
-    sntp_setservername(0, CONFIG_WILLOW_NTP_HOST);
-#endif
+
+    char *ntp_config = config_get_char("ntp_config", DEFAULT_NTP_CONFIG);
+    if (strcmp(ntp_config, "DHCP") == 0) {
+        ESP_LOGI(TAG, "Using DHCP SNTP server");
+        sntp_servermode_dhcp(1);
+    } else if (strcmp(ntp_config, "Host") == 0) {
+        char *ntp_host = config_get_char("ntp_host", DEFAULT_NTP_HOST);
+        ESP_LOGI(TAG, "Using configured SNTP server '%s'", ntp_host);
+        sntp_setservername(0, ntp_host);
+        free(ntp_host);
+    }
+    free(ntp_config);
     sntp_set_time_sync_notification_cb(cb_sntp);
     sntp_init();
 

main/Kconfig.projbuild
@@ -141,28 +141,6 @@ config WILLOW_LCD_BRIGHTNESS
     help
         LCD Brightness from 0-1023
 
-choice WILLOW_NTP
-    prompt "NTP Configuration"
-    default WILLOW_NTP_USE_HOST
-
-    config WILLOW_NTP_USE_HOST
-        bool "Use specific NTP server"
-        help
-            NTP host for obtaining system time
-
-    config WILLOW_NTP_USE_DHCP
-        bool "Use NTP server provided by DHCP"
-        help
-            Override NTP server with host provided via DHCP
-endchoice
-
-config WILLOW_NTP_HOST
-    depends on WILLOW_NTP_USE_HOST
-    string "Willow NTP host"
-    default "pool.ntp.org"
-    help
-        NTP host for obtaining system time
-
     menu "Advanced settings"
 
         config WILLOW_VAD_TIMEOUT

sdkconfig.willow
@@ -158,9 +158,6 @@ CONFIG_WILLOW_USE_ENDPOINT_HOMEASSISTANT=y
 CONFIG_WILLOW_TIMEZONE="UTC+5"
 CONFIG_WILLOW_VOLUME=15
 CONFIG_WILLOW_LCD_BRIGHTNESS=500
-CONFIG_WILLOW_NTP_USE_HOST=y
-# CONFIG_WILLOW_NTP_USE_DHCP is not set
-CONFIG_WILLOW_NTP_HOST="pool.ntp.org"
 
 #
 # Advanced settings

commit c0eea605838a9e54fb052eedebb8d3965da7cc90

network: do not free ntp_host
We cannot free ntp_host as this will result in the SNTP service
accessing freed memory and as a result NTP will not have a correct
server to synchronize time from.

Fixes: e9be16b29168 ("network: use config_get for NTP")

main/network.c
@@ -71,7 +71,6 @@ esp_err_t init_sntp(void)
         char *ntp_host = config_get_char("ntp_host", DEFAULT_NTP_HOST);
         ESP_LOGI(TAG, "Using configured SNTP server '%s'", ntp_host);
         sntp_setservername(0, ntp_host);
-        free(ntp_host);
     }
     free(ntp_config);
     sntp_set_time_sync_notification_cb(cb_sntp);