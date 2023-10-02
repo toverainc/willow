toverainc/willow

commit 0294c9ae0b858a8854a4ef4df97b4d4d7d726585

config: add config_get functions for types we use
For this we need to make the cJSON pointer a global variable.

main/config.c
@@ -1,11 +1,12 @@
 #include <stdio.h>
+#include <string.h>
 #include <sys/stat.h>
 
-#include "cJSON.h"
 #include "esp_log.h"
 #include "esp_spiffs.h"
 #include "esp_system.h"
 
+#include "config.h"
 #include "was.h"
 
 #define CONFIG_PATH "/spiffs/user/config/willow.json"
@@ -13,6 +14,7 @@
 static const char *TAG = "WILLOW/CONFIG";
 
 bool config_valid = false;
+cJSON *wc = NULL; // TODO: cJSON_Delete() after all config_get_* calls
 
 static char *config_read(void)
 {
@@ -45,6 +47,38 @@ close:
     return config;
 }
 
+bool config_get_bool(char *key, const bool default_value)
+{
+    bool ret = default_value;
+    cJSON *val = cJSON_GetObjectItemCaseSensitive(wc, key);
+    if (val != NULL && cJSON_IsBool(val)) {
+        ret = cJSON_IsTrue(val) ? true : false;
+    }
+    return ret;
+}
+
+char *config_get_char(const char *key, const char *default_value)
+{
+    char *ret = NULL;
+    cJSON *val = cJSON_GetObjectItemCaseSensitive(wc, key);
+    if (val != NULL && cJSON_IsString(val) && val->valuestring != NULL) {
+        ret = strndup(val->valuestring, strlen(val->valuestring));
+    } else {
+        ret = strndup(default_value, strlen(default_value));
+    }
+    return ret;
+}
+
+int config_get_int(char *key, const int default_value)
+{
+    int ret = -1;
+    cJSON *val = cJSON_GetObjectItemCaseSensitive(wc, key);
+    if (cJSON_IsNumber(val)) {
+        ret = val->valueint;
+    }
+    return ret;
+}
+
 void config_parse(void)
 {
     char *config = config_read();
@@ -54,8 +88,8 @@ void config_parse(void)
         return;
     }
 
-    cJSON *cjson = cJSON_Parse(config);
-    if (cjson == NULL) {
+    wc = cJSON_Parse(config);
+    if (wc == NULL) {
         const char *eptr = cJSON_GetErrorPtr();
         if (eptr != NULL) {
             ESP_LOGE(TAG, "error parsing config file: %s\n", eptr);
@@ -65,12 +99,11 @@ void config_parse(void)
 
     config_valid = true;
 
-    json = cJSON_Print(cjson);
+    json = cJSON_Print(wc);
     ESP_LOGI(TAG, "parsed config file:");
     printf("%s\n", json);
 
 cleanup:
-    cJSON_Delete(cjson);
     free(config);
 }
 

main/config.h
@@ -1,3 +1,9 @@
+#include "cJSON.h"
+
 bool config_valid;
+cJSON *wc;
+bool config_get_bool(char *key, const bool default_value);
+char *config_get_char(const char *key, const char *default_value);
+int config_get_int(char *key, const int default_value);
 void config_parse(void);
 void config_write(const char *data);

commit a7b7830a057f440246fe6529434c3c9373335300

config: fix default value for config_get_bool
Fixes: 0294c9ae0b85 ("config: add config_get functions for types we use")

main/config.c
@@ -62,6 +62,8 @@ bool config_get_bool(char *key, const bool default_value)
     cJSON *val = cJSON_GetObjectItemCaseSensitive(wc, key);
     if (val != NULL && cJSON_IsBool(val)) {
         ret = cJSON_IsTrue(val) ? true : false;
+    } else {
+        ret = default_value;
     }
     return ret;
 }