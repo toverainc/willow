toverainc/willow

commit b0839ca520f9bba17cc6c317e3193b4030079865

network: use config_get_char for timezone

main/network.c
@@ -13,6 +13,7 @@
 
 #define DEFAULT_NTP_CONFIG "Host"
 #define DEFAULT_NTP_HOST   "pool.ntp.org"
+#define DEFAULT_TIMEZONE   "America/Menominee"
 
 #define HOSTNAME_SIZE 20
 #define MAC_ADDR_SIZE 6
@@ -55,7 +56,9 @@ void set_hostname(esp_mac_type_t emt)
 esp_err_t init_sntp(void)
 {
     ESP_LOGI(TAG, "initializing SNTP client");
-    setenv("TZ", CONFIG_WILLOW_TIMEZONE, 1);
+    char *timezone = config_get_char("timezone", DEFAULT_TIMEZONE);
+    setenv("TZ", timezone, 1);
+    free(timezone);
     tzset();
     sntp_setoperatingmode(SNTP_OPMODE_POLL);
 

main/Kconfig.projbuild
@@ -47,12 +47,6 @@ choice WILLOW_WAKE_WORD
             Say Hi Lexin to wake Willow
 endchoice
 
-config WILLOW_TIMEZONE
-    string "Timezone"
-    default "UTC+5"
-    help
-        Willow device timezone
-
     menu "Advanced settings"
 
         config WILLOW_ETHERNET

sdkconfig.willow
@@ -152,7 +152,6 @@ CONFIG_WIFI_PASSWORD="mypassword"
 CONFIG_WILLOW_WAKE_WORD_HIESP=y
 # CONFIG_WILLOW_WAKE_WORD_ALEXA is not set
 # CONFIG_WILLOW_WAKE_WORD_HILEXIN is not set
-CONFIG_WILLOW_TIMEZONE="UTC+5"
 
 #
 # Debugging

commit cbe3579b7d4b5d178b6d49515575bcfa5cd3c41c

network: use POSIX string for default timezone
When there is no timezone set in willow.json we fall back to
DEFAULT_TIMEZONE. This has to be a POSIX timezone string.

Fixes: b0839ca520f9 ("network: use config_get_char for timezone")

main/network.c
@@ -17,7 +17,7 @@
 
 #define DEFAULT_NTP_CONFIG "Host"
 #define DEFAULT_NTP_HOST   "pool.ntp.org"
-#define DEFAULT_TIMEZONE   "America/Menominee"
+#define DEFAULT_TIMEZONE   "CST6CDT,M3.2.0,M11.1.0"
 
 #ifndef INET6_ADDRSTRLEN
 #define INET6_ADDRSTRLEN 48