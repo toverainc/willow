f432ae3231d7c2a3331e72f0086b2c7e637129e8
Stijn Tintel <stijn@linux-ipv6.be>
2023-10-26 17:30:05 +0300

    display: add task for display backlight strobe

--- main/display.h
+++ main/display.h
@@ -1,6 +1,11 @@
 #include "periph_lcd.h"
 
+typedef struct {
+    int period_ms;
+} willow_strobe_parms_t;
+
 extern esp_lcd_panel_handle_t hdl_lcd;
 
 esp_err_t init_display(void);
+void display_backlight_strobe_task(void *data);
 void display_set_backlight(const bool on, const bool max);
