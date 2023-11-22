toverainc/willow

commit 349a8e47ebe7cf42ce7f5a605770360d3b9427ac

treewide: define display timeout in seconds
Define the display timeout in seconds rather than in microseconds, in
preparation of making the timeout dynamically configurable.

main/audio.c
@@ -274,7 +274,7 @@ static esp_err_t cb_ar_event(audio_rec_evt_t *are, void *data)
                 lvgl_port_unlock();
             }
 
-            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
             break;
         case AUDIO_REC_WAKEUP_END:
             ESP_LOGI(TAG, "AUDIO_REC_WAKEUP_END");
@@ -292,7 +292,7 @@ static esp_err_t cb_ar_event(audio_rec_evt_t *are, void *data)
             recorder_sr_wakeup_result_t *wake_data = are->event_data;
             ESP_LOGI(TAG, "wake volume: %f", wake_data->data_volume);
             send_wake_start(wake_data->data_volume);
-            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, true);
+            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, true);
 
             speech_rec_mode = config_get_char("speech_rec_mode", DEFAULT_SPEECH_REC_MODE);
 
@@ -356,7 +356,7 @@ static esp_err_t cb_ar_event(audio_rec_evt_t *are, void *data)
                     lv_label_set_text(lbl_ln2, lookup_cmd_multinet(command_id));
                     lvgl_port_unlock();
                 }
-                reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+                reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
 #else
                 ESP_LOGE(TAG, "multinet not supported but enabled in config");
 #endif
@@ -785,7 +785,7 @@ static void at_read(void *data)
                         lv_obj_add_flag(btn_cancel, LV_OBJ_FLAG_HIDDEN);
                         lvgl_port_unlock();
                     }
-                    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+                    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
                     break;
                 default:
                     printf("at_read(): invalid msg '%d'\n", msg);

main/config.c
@@ -151,7 +151,7 @@ close:
         lv_obj_clear_flag(lbl_ln3, LV_OBJ_FLAG_HIDDEN);
         lvgl_port_unlock();
     }
-    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, true);
+    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, true);
     display_set_backlight(true);
     restart_delayed();
 }

main/endpoint/hass.c
@@ -166,7 +166,7 @@ end:
                     free(hir.speech);
                 }
 
-                reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+                reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
 
 cleanup:
                 cJSON_Delete(cjson);
@@ -367,7 +367,7 @@ http_error:
         lvgl_port_unlock();
     }
 
-    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
     free(body);
 
     free(url);

main/endpoint/openhab.c
@@ -87,7 +87,7 @@ end:
         lvgl_port_unlock();
     }
 
-    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
 
     free(body);
 }

main/endpoint/rest.c
@@ -88,7 +88,7 @@ void rest_send(const char *data)
         lvgl_port_unlock();
     }
 
-    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
 
     free(body);
 }

main/main.c
@@ -182,7 +182,7 @@ err_nvs:
     // we can also still crash in the while loop below - this should be improved
     ESP_ERROR_CHECK_WITHOUT_ABORT(esp_ota_mark_app_valid_cancel_rollback());
 
-    ESP_ERROR_CHECK_WITHOUT_ABORT(reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false));
+    ESP_ERROR_CHECK_WITHOUT_ABORT(reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false));
 
 #ifdef CONFIG_WILLOW_DEBUG_RUNTIME_STATS
     xTaskCreate(&task_debug_runtime_stats, "dbg_runtime_stats", 4 * 1024, NULL, 0, NULL);

main/ota.c
@@ -181,7 +181,7 @@ err:
 
 void ota_start(char *url)
 {
-    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, true);
+    reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, true);
     if (lvgl_port_lock(lvgl_lock_timeout)) {
         lv_obj_add_flag(lbl_ln1, LV_OBJ_FLAG_HIDDEN);
         lv_obj_add_flag(lbl_ln2, LV_OBJ_FLAG_HIDDEN);

main/slvgl.c
@@ -57,11 +57,11 @@ void cb_scr(lv_event_t *ev)
     // printf("cb_scr\n");
     switch (lv_event_get_code(ev)) {
         case LV_EVENT_RELEASED:
-            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, false);
+            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, false);
             break;
 
         case LV_EVENT_PRESSED:
-            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT_US, true);
+            reset_timer(hdl_display_timer, DISPLAY_TIMEOUT, true);
             display_set_backlight(true);
             break;
 

commit 90bbb882a5b789732611da2a9b97b3ec8e27b24b

audio: fix session timer
In commit 349a8e47ebe7 ("treewide: define display timeout in seconds"),
we changed reset_timer to convert the timeout to microseconds inside the
function, but we forgot to change the reset_timer call for the session
timer. As a result, the session timer was waiting for millions of
seconds rather than just the number of seconds in the config.

Fixes: 349a8e47ebe7 ("treewide: define display timeout in seconds")

main/audio.c
@@ -315,8 +315,7 @@ static esp_err_t cb_ar_event(audio_rec_evt_t *are, void *data)
             speech_rec_mode = config_get_char("speech_rec_mode", DEFAULT_SPEECH_REC_MODE);
 
             if (strcmp(speech_rec_mode, "WIS") == 0) {
-                reset_timer(hdl_sess_timer, config_get_int("stream_timeout", DEFAULT_STREAM_TIMEOUT) * 1000 * 1000,
-                            false);
+                reset_timer(hdl_sess_timer, config_get_int("stream_timeout", DEFAULT_STREAM_TIMEOUT), false);
             }
             if (lvgl_port_lock(lvgl_lock_timeout)) {
                 lv_obj_add_flag(lbl_ln1, LV_OBJ_FLAG_HIDDEN);