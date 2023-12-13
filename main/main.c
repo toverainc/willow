toverainc/willow

commit 36ee1f70bf1d23d2cf654afbd2a95db448e73699

main: read Wi-Fi credentials from NVS
Add a state enum to track where we are in the boot phase. When we fail
to read the Wi-Fi credentials from NVS, we skip connecting to Wi-Fi and
WAS, and wait forever.

As of this commit, the NVS partition will need to have a namezone "WIFI"
with 2 keys "PSK" and "SSID", or you will no longer be able to connected
to Wi-Fi.

NVS partition can be generated with the instructions in NOTES.md, or on
https://www.linux-ipv6.be/esp-nvs/ (temp URL).

The resulting NVS partition can then be flashed with this command:
esptool.py --port /dev/ttyACM0 write_flash 0x9000 ~/tmp/nvs.bin

Alternatively, you can use https://espressif.github.io/esptool-js/.
Make sure to set the offset to 0x9000.

main/main.c
@@ -633,6 +633,8 @@ static esp_err_t init_spiffs_user(void)
 
 void app_main(void)
 {
+    state = STATE_INIT;
+
 #ifdef CONFIG_WILLOW_DEBUG_LOG
     esp_log_level_set("*", ESP_LOG_DEBUG);
 #else
@@ -669,7 +671,30 @@ void app_main(void)
 #ifdef CONFIG_WILLOW_ETHERNET
     init_ethernet();
 #else
-    init_wifi(CONFIG_WIFI_PASSWORD, CONFIG_WIFI_SSID);
+    nvs_handle_t hdl_nvs;
+    ret = nvs_open("WIFI", NVS_READONLY, &hdl_nvs);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "failed to open NVS namespace WIFI: %s", esp_err_to_name(err));
+        goto err_nvs;
+    }
+
+    char psk[64];
+    size_t sz = sizeof(psk);
+    ret = nvs_get_str(hdl_nvs, "PSK", psk, &sz);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "failed to get PSK from NVS namespace WIFI: %s", esp_err_to_name(err));
+        goto err_nvs;
+    }
+
+    char ssid[33];
+    sz = sizeof(ssid);
+    ret = nvs_get_str(hdl_nvs, "SSID", ssid, &sz);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "failed to get PSK from NVS namespace WIFI: %s", esp_err_to_name(err));
+        goto err_nvs;
+    }
+    state = STATE_NVS_OK;
+    init_wifi(psk, ssid);
 #endif
     ret = init_was();
     if (ret != ESP_OK) {
@@ -680,12 +705,23 @@ void app_main(void)
             lv_label_set_text_static(lbl_ln3, "WAS initialization failed.");
             lvgl_port_unlock();
         }
-        // wait "indefinitely"
-        vTaskDelay(portMAX_DELAY);
     }
 
     if (!config_valid) {
         request_config();
+    }
+
+// we jump over WAS initialization was without Wi-Fi this will never work
+err_nvs:
+    if (state < STATE_NVS_OK) {
+        if (ld == NULL) {
+            lvgl_port_lock(0);
+            lv_label_set_text_static(lbl_ln2, "Fatal error!");
+            lv_label_set_text_static(lbl_ln3, "Failed to read NVS partition.");
+            lvgl_port_unlock();
+        }
+        // wait "indefinitely"
+        vTaskDelay(portMAX_DELAY);
     }
 
     init_sntp();

main/shared.h
@@ -8,6 +8,15 @@
 
 bool recording;
 
+enum willow_state {
+    STATE_INIT = 0,
+    STATE_NVS_OK,
+    STATE_CONFIG_OK,
+    STATE_READY,
+};
+
+enum willow_state state;
+
 esp_audio_handle_t hdl_ea;
 esp_lcd_panel_handle_t hdl_lcd;
 esp_periph_set_handle_t hdl_pset;

commit 19f94e21c8142c44d1cd273fe05449674ee6ff9b

main: fix error printing in app_main
We're assigning the return value of several functions to the variable
ret, but when there's an error we print the name of err. As app_main
doesn't return anything, drop the ret variable completely and use err
everywhere.

Fixes: 36ee1f70bf1d ("main: read Wi-Fi credentials from NVS")
Fixes: ae4314713b8b ("main: get WAS URL from NVS")

main/main.c
@@ -67,7 +67,7 @@ static esp_err_t init_spiffs_user(void)
 void app_main(void)
 {
     state = STATE_INIT;
-    esp_err_t ret;
+    esp_err_t err;
 
     init_logging();
     ESP_LOGI(TAG, "Starting up! Please wait...");
@@ -84,7 +84,7 @@ void app_main(void)
 
     ESP_ERROR_CHECK(esp_netif_init());
 
-    esp_err_t err = nvs_flash_init();
+    err = nvs_flash_init();
     if (err == ESP_ERR_NVS_NO_FREE_PAGES) {
         // NVS partition was truncated and needs to be erased
         // Retry nvs_flash_init
@@ -96,44 +96,44 @@ void app_main(void)
     init_ethernet();
 #else
     nvs_handle_t hdl_nvs;
-    ret = nvs_open("WIFI", NVS_READONLY, &hdl_nvs);
-    if (ret != ESP_OK) {
+    err = nvs_open("WIFI", NVS_READONLY, &hdl_nvs);
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "failed to open NVS namespace WIFI: %s", esp_err_to_name(err));
         goto err_nvs;
     }
 
     char psk[64];
     size_t sz = sizeof(psk);
-    ret = nvs_get_str(hdl_nvs, "PSK", psk, &sz);
-    if (ret != ESP_OK) {
+    err = nvs_get_str(hdl_nvs, "PSK", psk, &sz);
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "failed to get PSK from NVS namespace WIFI: %s", esp_err_to_name(err));
         goto err_nvs;
     }
 
     char ssid[33];
     sz = sizeof(ssid);
-    ret = nvs_get_str(hdl_nvs, "SSID", ssid, &sz);
-    if (ret != ESP_OK) {
+    err = nvs_get_str(hdl_nvs, "SSID", ssid, &sz);
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "failed to get PSK from NVS namespace WIFI: %s", esp_err_to_name(err));
         goto err_nvs;
     }
     init_wifi(psk, ssid);
 #endif
 
-    ret = nvs_open("WAS", NVS_READONLY, &hdl_nvs);
-    if (ret != ESP_OK) {
+    err = nvs_open("WAS", NVS_READONLY, &hdl_nvs);
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "failed to open NVS namespace WAS: %s", esp_err_to_name(err));
         goto err_nvs;
     }
     sz = sizeof(was_url);
-    ret = nvs_get_str(hdl_nvs, "URL", was_url, &sz);
-    if (ret != ESP_OK) {
+    err = nvs_get_str(hdl_nvs, "URL", was_url, &sz);
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "failed to get WASL URL from NVS namespace WAS: %s", esp_err_to_name(err));
         goto err_nvs;
     }
     state = STATE_NVS_OK;
-    ret = init_was();
-    if (ret != ESP_OK) {
+    err = init_was();
+    if (err != ESP_OK) {
         ESP_LOGE(TAG, "failed to initialize Willow Application Server connection");
         ui_pr_err("Fatal error!", "WAS initialization failed.");
     }