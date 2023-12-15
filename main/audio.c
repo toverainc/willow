e5a68caeb02ac4f7a307410ecae3ee84a1df5326
Stijn Tintel <stijn@linux-ipv6.be>
2023-12-15 20:15:17 +0200

    audio: deinit SR recorder in deinit_audio

--- main/audio.c
+++ main/audio.c
@@ -1103,6 +1103,9 @@ void deinit_audio(void)
     if (hdl_ar != NULL) {
         audio_recorder_destroy(hdl_ar);
     }
+    if (hdl_sr != NULL) {
+        recorder_sr_destroy(hdl_sr);
+    }
     if (hdl_at != NULL) {
         vTaskDelete(hdl_at);
     }
