2aad1b376dd34249a5c860874ef8143dd0e61b17
Stijn Tintel <stijn@linux-ipv6.be>
2023-12-15 22:43:29 +0200

    ota: add support for OTA of SR model partition

--- main/ota.c
+++ main/ota.c
@@ -5,6 +5,7 @@
 #include "esp_log.h"
 #include "esp_lvgl_port.h"
 #include "esp_ota_ops.h"
+#include "esp_partition.h"
 #include "esp_task_wdt.h"
 #include "esp_timer.h"
 #include "lvgl.h"
@@ -169,6 +170,108 @@ err:
     vTaskDelete(NULL);
 }
 
+void srmodels_ota_task(void *data)
+{
+    bool started = false;
+    char *url = (char *)data;
+    const esp_partition_t *pt_srmodels = esp_partition_find_first(ESP_PARTITION_TYPE_ANY, ESP_PARTITION_SUBTYPE_ANY, "model");
+    esp_err_t ret = ESP_OK;
+    esp_http_client_handle_t hdl_hc = init_http_client();
+    int http_status = 0, ota_len = 0, ota_size = 0, read = 0;
+
+    if (pt_srmodels == NULL) {
+        ESP_LOGE(TAG, "srmodels partition not found, aborting srmodel update");
+        goto err;
+    }
+
+    ESP_LOGI(TAG, "downloading srmodels OTA from %s", url);
+
+    ret = esp_http_client_set_url(hdl_hc, url);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "failed to set srmodels OTA URL: %s", esp_err_to_name(ret));
+        goto err;
+    }
+
+    ret = esp_http_client_open(hdl_hc, 0);
+    if (ret != ESP_OK) {
+        ESP_LOGE(TAG, "failed to open HTTP connection: %s", esp_err_to_name(ret));
+        goto err;
+    }
+
+    ota_size = esp_http_client_fetch_headers(hdl_hc);
+    ESP_LOGI(TAG, "update size: %d byte", ota_size);
+
+    http_status = esp_http_client_get_status_code(hdl_hc);
+    if (http_status != 200) {
+        ESP_LOGE(TAG, "HTTP error (%d), aborting srmodel update", http_status);
+        goto err;
+    }
+
+    while (true) {
+        read = esp_http_client_read(hdl_hc, ota_data, BUFSIZE);
+
+        if (read < 0) {
+            goto err;
+        }
+
+        if (read == 0) {
+            if (errno == ECONNRESET || errno == ENOTCONN) {
+                ESP_LOGE(TAG, "connection problem occurred (%d)", errno);
+                goto err;
+            }
+
+            if (esp_http_client_is_complete_data_received(hdl_hc)) {
+                ESP_LOGI(TAG, "srmodels OTA download completed");
+                break;
+            }
+        }
+
+        if (read > 0) {
+            if (started == false) {
+                    // OTA begin triggers TWDT due to partition erase taking a long time
+                    esp_task_wdt_config_t ota_config = {
+                        .timeout_ms = 30000,
+                        .idle_core_mask = 0,
+                        .trigger_panic = true,
+                    };
+                    esp_task_wdt_reconfigure(&ota_config);
+
+                    ESP_LOGI(TAG, "starting srmodels OTA");
+                    esp_partition_erase_range(pt_srmodels, 0, pt_srmodels->size);
+                    started = true;
+            }
+            ret = esp_partition_write(pt_srmodels, ota_len, (const void *)ota_data, read);
+            if (ret != ESP_OK) {
+                ESP_LOGE(TAG, "failed to write srmodels OTA data: %s", esp_err_to_name(ret));
+                goto err;
+            }
+            ota_len += read;
+            ESP_LOGD(TAG, "srmodels OTA data written: %d byte", ota_len);
+            // add a delay to allow feeding TWDT
+            vTaskDelay(10 / portTICK_PERIOD_MS);
+        }
+    }
+
+    ESP_LOGI(TAG, "total srmodels OTA data written: %d byte", ota_len);
+
+    ESP_LOGI(TAG, "srmodels OTA completed, restarting");
+    if (lvgl_port_lock(lvgl_lock_timeout)) {
+        lv_label_set_text_static(lbl_ln3, "Upgrade Done");
+        lvgl_port_unlock();
+    }
+    restart_delayed();
+err:
+    esp_http_client_close(hdl_hc);
+    esp_http_client_cleanup(hdl_hc);
+    ESP_LOGI(TAG, "srmodels OTA failed, restarting");
+    if (lvgl_port_lock(lvgl_lock_timeout)) {
+        lv_label_set_text_static(lbl_ln3, "Upgrade Failed");
+        lvgl_port_unlock();
+    }
+    restart_delayed();
+    vTaskDelete(NULL);
+}
+
 void ota_start(char *url)
 {
     reset_timer(hdl_display_timer, config_get_int("display_timeout", DEFAULT_DISPLAY_TIMEOUT), true);
@@ -189,4 +292,26 @@ void ota_start(char *url)
 
     vTaskDelay(1000 / portTICK_PERIOD_MS);
     xTaskCreate(&ota_task, "ota_task", 8192, url, 5, NULL);
+}
+
+void srmodels_ota_start(char *url)
+{
+    reset_timer(hdl_display_timer, config_get_int("display_timeout", DEFAULT_DISPLAY_TIMEOUT), true);
+    if (lvgl_port_lock(lvgl_lock_timeout)) {
+        lv_obj_add_flag(lbl_ln1, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag(lbl_ln2, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag(lbl_ln4, LV_OBJ_FLAG_HIDDEN);
+        lv_obj_add_flag(lbl_ln5, LV_OBJ_FLAG_HIDDEN);
+        lv_label_set_text_static(lbl_ln3, "Updating SR models");
+        lv_obj_clear_flag(lbl_ln3, LV_OBJ_FLAG_HIDDEN);
+        lvgl_port_unlock();
+    }
+    display_set_backlight(true, false);
+
+    deinit_audio();
+    deinit_hass();
+    deinit_was();
+
+    vTaskDelay(1000 / portTICK_PERIOD_MS);
+    xTaskCreate(&srmodels_ota_task, "srmodels_ota_task", 8192, url, 5, NULL);
 }
--- main/ota.h
+++ main/ota.h
@@ -1,1 +1,2 @@
 void ota_start(char *url);
+void srmodels_ota_start(char *url);
