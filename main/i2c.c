toverainc/willow

commit 544ff38630a7f57d054e69b2ba5e104b9879f6ca

i2c: implement i2c_probe function
And use it to probe for the I2C addresses of touch controllers used in
the ESP32-S3-Box variants supported by Willow. This is in preparation of
switching back to upstream ESP-ADF instead of our fork.

The i2c_bus_probe_addr function in ESP-ADF's i2c_bus driver rejects
addresses that use more than 7 bits, but does not set bit 8 to indicate
that we want to write from master to slave.

main/i2c.c
@@ -1,0 +1,16 @@
+#include "i2c.h"
+
+esp_err_t i2c_probe(i2c_port_t port, uint8_t addr)
+{
+    esp_err_t ret = ESP_ERR_NOT_FOUND;
+
+    i2c_cmd_handle_t hdl_i2c_cmd = i2c_cmd_link_create();
+    i2c_master_start(hdl_i2c_cmd);
+    i2c_master_write_byte(hdl_i2c_cmd, (addr << 1) | I2C_MASTER_WRITE, I2C_MASTER_ACK);
+    i2c_master_stop(hdl_i2c_cmd);
+
+    ret = i2c_master_cmd_begin(port, hdl_i2c_cmd, 5000 / portTICK_PERIOD_MS);
+    i2c_cmd_link_delete(hdl_i2c_cmd);
+
+    return ret;
+}

main/CMakeLists.txt
@@ -6,6 +6,7 @@ set(COMPONENT_SRCS
     "endpoint/openhab.c"
     "endpoint/rest.c"
     "http.c"
+    "i2c.c"
     "input.c"
     "log.c"
     "lv_img_hand_left.c"

main/i2c.h
@@ -1,0 +1,3 @@
+#include "driver/i2c.h"
+
+esp_err_t i2c_probe(i2c_port_t port, uint8_t addr);

main/slvgl.c
@@ -7,13 +7,13 @@
 #include "esp_log.h"
 #include "esp_lvgl_port.h"
 #include "esp_timer.h"
-#include "i2c_bus.h"
 #include "lvgl.h"
 #include "periph_lcd.h"
 
 #include "audio.h"
 #include "config.h"
 #include "display.h"
+#include "i2c.h"
 #include "system.h"
 #include "timer.h"
 
@@ -176,13 +176,13 @@ esp_err_t init_lvgl_touch(void)
 
     esp_lcd_panel_io_i2c_config_t cfg_io_lt;
 
-    if (i2c_bus_probe_addr(hdl_i2c_bus, ESP_LCD_TOUCH_IO_I2C_GT911_ADDRESS << 1) == ESP_OK) {
+    if (i2c_probe(I2C_NUM_0, ESP_LCD_TOUCH_IO_I2C_GT911_ADDRESS) == ESP_OK) {
         cfg_io_lt = cfg_lpiic_gt911(ESP_LCD_TOUCH_IO_I2C_GT911_ADDRESS);
         touch_type = TOUCH_GT911;
-    } else if (i2c_bus_probe_addr(hdl_i2c_bus, ESP_LCD_TOUCH_IO_I2C_GT911_ADDRESS_BACKUP << 1) == ESP_OK) {
+    } else if (i2c_probe(I2C_NUM_0, ESP_LCD_TOUCH_IO_I2C_GT911_ADDRESS_BACKUP) == ESP_OK) {
         cfg_io_lt = cfg_lpiic_gt911(ESP_LCD_TOUCH_IO_I2C_GT911_ADDRESS_BACKUP);
         touch_type = TOUCH_GT911;
-    } else if (i2c_bus_probe_addr(hdl_i2c_bus, ESP_LCD_TOUCH_IO_I2C_TT21100_ADDRESS << 1) == ESP_OK) {
+    } else if (i2c_probe(I2C_NUM_0, ESP_LCD_TOUCH_IO_I2C_TT21100_ADDRESS) == ESP_OK) {
         cfg_io_lt = cfg_lpiic_tt21100();
         cfg_lt.flags.mirror_x = true;
         touch_type = TOUCH_TT21100;

commit 0c7f281594e7a3cd42bc76bf34f73744147b9ebc

i2c: fix i2c_probe function
The i2c_master_write_byte function expects a boolean as 3rd argument,
not i2c_ack_type_t. The I2C_MASTER_ACK variant of this enum is 0x0,
which disables the I2C ACK bit. However, setting the ACK bit is crucial
for probing devices on the I2C bus.

Fixes: 544ff38630a7 ("i2c: implement i2c_probe function")

main/i2c.c
@@ -30,7 +30,7 @@ esp_err_t i2c_probe(i2c_port_t port, uint8_t addr)
 
     i2c_cmd_handle_t hdl_i2c_cmd = i2c_cmd_link_create();
     i2c_master_start(hdl_i2c_cmd);
-    i2c_master_write_byte(hdl_i2c_cmd, (addr << 1) | I2C_MASTER_WRITE, I2C_MASTER_ACK);
+    i2c_master_write_byte(hdl_i2c_cmd, (addr << 1) | I2C_MASTER_WRITE, true);
     i2c_master_stop(hdl_i2c_cmd);
 
     ret = i2c_master_cmd_begin(port, hdl_i2c_cmd, 5000 / portTICK_PERIOD_MS);