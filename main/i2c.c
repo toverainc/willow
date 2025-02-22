dee0fa80473c9a83eab70080587fed5e338ef203
Stijn Tintel <stijn@linux-ipv6.be>
2025-02-22 16:01:45 +0200

    i2c: fix i2c_probe function
    The i2c_master_write_byte function expects a boolean as 3rd argument,
    not i2c_ack_type_t. The I2C_MASTER_ACK variant of this enum is 0x0,
    which disables the I2C ACK bit. However, setting the ACK bit is crucial
    for probing devices on the I2C bus.
    
    Fixes: 544ff38630a7 ("i2c: implement i2c_probe function")

--- main/i2c.c
+++ main/i2c.c
@@ -30,7 +30,7 @@ esp_err_t i2c_probe(i2c_port_t port, uint8_t addr)
 
     i2c_cmd_handle_t hdl_i2c_cmd = i2c_cmd_link_create();
     i2c_master_start(hdl_i2c_cmd);
-    i2c_master_write_byte(hdl_i2c_cmd, (addr << 1) | I2C_MASTER_WRITE, I2C_MASTER_ACK);
+    i2c_master_write_byte(hdl_i2c_cmd, (addr << 1) | I2C_MASTER_WRITE, true);
     i2c_master_stop(hdl_i2c_cmd);
 
     ret = i2c_master_cmd_begin(port, hdl_i2c_cmd, 5000 / portTICK_PERIOD_MS);
