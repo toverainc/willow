toverainc/willow

commit 390533f1c51a227e5cbc2ff58c2f23e845a53eb2

CI: don't trigger workflow on push
Commit 2fce5e732f1c ("CI: detect changed files since last remote
commit") only fixed the problem when triggering worklfow on a pull
request event. The PR to bump ESP-IDF to v5.2.3 still has failing checks
due to the build triggered by the push event not rebuilding the build
container first, so it fails because it's building with ESP-IDF v5.1.6
while the manifest file now has a dependency on >= 5.2.0.

There appear to be some solutions to skip building on push when a pull
request exists for the branch that is being pushed to, but it requires
some vague conditions. This all just feels like a hack for something
that should imo be basic functionality. Let's just disable the push
trigger and be done with it.

.github/workflows/meta.yml
@@ -6,7 +6,6 @@ env:
 
 on:
   pull_request:
-  push:
 
 jobs:
   trigger_workflow:

commit ee2881ee530faf04d2bbfd47488952ca653bde51

CI: trigger workflow on push again
If we don't trigger workflow on push, we will not build and push a new
container image when a PR is merged. Because of this, the container
image with the main tag is currently based on ESP-IDF v5.1, while we
already have code in the main branch that requires ESP-IDF v5.2.

We will also not publish a release anymore when pushing a tag.

Trigger workflow on push again, but only for the main and release
branches, and for any tag.

Also add an empty newline to Dockerfile to make sure we trigger
container build and push, so that the container image with the main tag
will be based on ESP-IDF v5.2.

Fixes: 390533f1c51a ("CI: don't trigger workflow on push")

.github/workflows/meta.yml
@@ -6,6 +6,12 @@ env:
 
 on:
   pull_request:
+  push:
+    branches:
+      - 'main'
+      - 'release/**'
+    tag:
+      - '*'
 
 jobs:
   trigger_workflow:

Dockerfile
@@ -31,3 +31,4 @@ ENV ADF_VER="v2.7"
 RUN \
     cd /opt/esp/idf && \
     curl https://raw.githubusercontent.com/espressif/esp-adf/$ADF_VER/idf_patches/idf_v5.2_freertos.patch | patch -p1
+