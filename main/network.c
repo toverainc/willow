6c1273e99cac1b8ced2d4bd0d27143bfbd24c317
Stijn Tintel <stijn@linux-ipv6.be>
2025-02-27 23:29:25 +0200

    network: log AP info, reason and rssi on disconnect event

--- main/network.c
+++ main/network.c
@@ -140,8 +140,12 @@ static void hdlr_ev_wifi(void *arg, esp_event_base_t ev_base, int32_t ev_id, voi
             break;
 
         case WIFI_EVENT_STA_DISCONNECTED:
+            wifi_event_sta_disconnected_t *ev_data_disconnected = (wifi_event_sta_disconnected_t *)data;
+            ESP_LOGI(TAG, "disconnected from AP (BSSID='" MACSTR "' SSID='%s' reason='%" PRIu8 "' rssi='%'" PRId8 "')",
+                     MAC2STR(ev_data_disconnected->bssid), ev_data_disconnected->ssid, ev_data_disconnected->reason,
+                     ev_data_disconnected->rssi);
             if (!restarting) {
-                ESP_LOGI(TAG, "disconnected from AP, retrying");
+                ESP_LOGI(TAG, "reconnecting");
                 esp_wifi_connect();
             }
             break;
