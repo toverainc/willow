c63681448dd37ba17657c5b42bf6e8f3b682a08f
Stijn Tintel <stijn@linux-ipv6.be>
2025-05-27 00:35:10 +0300

    network: scan all Wi-Fi channels
    The esp_wifi driver uses WIFI_FAST_SCAN by default, which means it will
    stop scanning after it finds a matching AP. This is the main reason
    Willow sometimes connect to an AP with a terrible signal strength.
    
    Change the scan method to WIFI_ALL_CHANNEL_SCAN so that all channels
    will be scanned. This seems to increase the connect time with 2 seconds,
    but this is an acceptable trade-off to provide a better user experience.
    Willow becomes unusable if the signal strength is too low.
    
    Reported-by: Ciaran Jessup <ciaranj@gmail.com>

--- main/network.c
+++ main/network.c
@@ -207,6 +207,7 @@ esp_err_t init_wifi(const char *psk, const char *ssid)
             .btm_enabled = 1,
             .mbo_enabled = 1,
             .rm_enabled = 1,
+            .scan_method = WIFI_ALL_CHANNEL_SCAN,
             .threshold = {
                 .authmode = WIFI_AUTH_WPA2_PSK,
             }
