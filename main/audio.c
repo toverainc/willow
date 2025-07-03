toverainc/willow

commit 67ff0351b7413e80636fa4cb277bc952d41e2e9f

audio: increase HTTP stream timeout after send
In commit af99de418c18 ("audio: reduce HTTP Stream timeout to 2s") we
changed the HTTP Stream timeout to 2s so we can indicate early when
we're not able to connect to WIS.

Unfortunately ESP HTTP Client only has a single timeout value, and we
now also abort the HTTP connection to WIS if inference takes longer than
2s. We want to keep the timeout reasonably low, as having to wait for a
STT response for too long isn't great for UX, but 2s makes it impossible
for people to test Willow with a local WIS on CPU or slow GPU.

Leave the initial timeout at 2s, but increase it to 10s on the
HTTP_STREAM_POST_REQUEST event, which happens after the HTTP client sent
header and body to WIS, but before fetching the response.

main/audio.c
@@ -59,7 +59,8 @@
 #define DEFAULT_WIS_TTS_URL         "https://infer.tovera.io/api/tts"
 #define DEFAULT_WIS_URL             "https://infer.tovera.io/api/willow"
 
-#define HTTP_STREAM_TIMEOUT_MS 2 * 1000
+#define HTTP_STREAM_TIMEOUT_MS              2 * 1000
+#define HTTP_STREAM_TIMEOUT_MS_POST_REQUEST 10 * 1000
 
 #define MULTINET_TWDT   30
 #define STR_WAKE_LEN    25
@@ -519,6 +520,7 @@ static esp_err_t hdl_ev_hs_to_api(http_stream_event_msg_t *msg)
 
         case HTTP_STREAM_POST_REQUEST:
             ESP_LOGI(TAG, "WIS HTTP client HTTP_STREAM_POST_REQUEST, write end chunked marker");
+            esp_http_client_set_timeout_ms(http, HTTP_STREAM_TIMEOUT_MS_POST_REQUEST);
             if (esp_http_client_write(http, "0\r\n\r\n", 5) <= 0) {
                 return ESP_FAIL;
             }

commit d459cbb34748be5f619a44a1a0aad3699915b760

audio: set HTTP stream timeout after connect
In commit 67ff0351b741 ("audio: increase HTTP stream timeout after
send") we increased the timeout for the HTTP stream to WIS STT after
send to 10s, to allow people with slow WIS to still use Willow. For some
reason this worked in the 0.3 releases, but it seems this doesn't work
anymore after all the bumps of Espressif components in the 0.4 releases.

The commit was wrong in the first place, as we should increase the
timeout after initial connect, but before we finish sending, as setting
it after we finished sending should result in the connection timing out
after 2s while sending.

Set the timeout on the HTTP_STREAM_ON_REQUEST event to fix this.

Fixes: 67ff0351b741 ("audio: increase HTTP stream timeout after send")

main/audio.c
@@ -556,6 +556,7 @@ static esp_err_t hdl_ev_hs_to_api(http_stream_event_msg_t *msg)
             return ESP_OK;
 
         case HTTP_STREAM_ON_REQUEST:
+            esp_http_client_set_timeout_ms(http, HTTP_STREAM_TIMEOUT_MS_POST_REQUEST);
             wlen = sprintf(len_buf, "%x\r\n", msg->buffer_len);
             if (esp_http_client_write(http, len_buf, wlen) <= 0) {
                 return ESP_FAIL;
@@ -572,7 +573,6 @@ static esp_err_t hdl_ev_hs_to_api(http_stream_event_msg_t *msg)
 
         case HTTP_STREAM_POST_REQUEST:
             ESP_LOGI(TAG, "WIS HTTP client HTTP_STREAM_POST_REQUEST, write end chunked marker");
-            esp_http_client_set_timeout_ms(http, HTTP_STREAM_TIMEOUT_MS_POST_REQUEST);
             if (esp_http_client_write(http, "0\r\n\r\n", 5) <= 0) {
                 return ESP_FAIL;
             }